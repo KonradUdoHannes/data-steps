a7bfe2f21494976a7317f331b923dcea33d3b238
konradudohannes <konrad.woelms@gmail.com>
2021-04-11 15:40:45 +0200

    added some tests for the step collection

--- data_steps/tests/test_single_frame.py
+++ data_steps/tests/test_single_frame.py
@@ -46,3 +46,68 @@ def test_step_collection_overview():
         pass 
     sc.add_step(sample_function,priority = 3)
     assert not sc.step_overview().empty
+
+def test_riority_adding():
+    sc = StepCollection()
+    def sample_function_low_prio(): pass
+    def sample_function_mid_prio(): pass
+    def sample_function_high_prio(): pass
+
+    sc.add_step(sample_function_low_prio,priority = 5)
+    sc.add_step(sample_function_high_prio,priority = 1)
+    sc.add_step(sample_function_mid_prio,priority = 3)
+
+    ordered_steps = sc.ordered_steps
+
+    assert ordered_steps[0].function.__name__ == 'sample_function_high_prio'
+    assert ordered_steps[1].function.__name__ == 'sample_function_mid_prio'
+    assert ordered_steps[2].function.__name__ == 'sample_function_low_prio'
+
+def test_reregister_no_doubles():
+    """Relevant for jupyter use cases.
+    """
+    sc = StepCollection()
+    def sample_function():
+        pass
+
+    sc.add_step(sample_function,priority = 5)
+    sc.add_step(sample_function,priority = 5)
+    assert len(sc.ordered_steps) == 1
+
+def test_priority_update():
+    """Relevant for jupyter use cases.
+    """
+    sc = StepCollection()
+    def sample_function():
+        pass
+
+    sc.add_step(sample_function,priority = 5)
+    sc.add_step(sample_function,priority = 3)
+    assert sc.ordered_steps[0].priority == 3
+
+def test_function_update():
+    """Relevant for jupyter use cases.
+    """
+    sc = StepCollection()
+    def sample_function():
+        return False
+
+    sc.add_step(sample_function,priority=5)
+    assert not sc.ordered_steps[0].function()
+
+    def sample_function():
+        return True
+
+    sc.add_step(sample_function,priority=5)
+    assert sc.ordered_steps[0].function()
+
+def test_remove_function():
+    """Relevant for jupyter use cases.
+    """
+    sc = StepCollection()
+    def sample_function(): pass
+
+    sc.add_step(sample_function,priority=5)
+    assert len(sc.ordered_steps) == 1
+    sc.remove_step(sample_function)
+    assert len(sc.ordered_steps) == 0
